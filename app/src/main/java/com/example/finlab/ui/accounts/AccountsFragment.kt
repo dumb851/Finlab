83b9b01f82e6e21643ad9e582222179c28c202ef
Uzbekov_r <uzbr851@gmail.com>
2019-12-16 19:05:16 +0300

    commit

---
+++ app/src/main/java/com/example/finlab/ui/accounts/AccountsFragment.kt
@@ -0,0 +1,25 @@
+package com.example.finlab.ui.accounts
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.finlab.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class AccountsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_accounts, container, false)
+    }
+
+
+}
