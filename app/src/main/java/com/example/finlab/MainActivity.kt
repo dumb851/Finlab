f6a708516d8acec4be93468fc93d30df6237b60d
Uzbekov_r <uzbr851@gmail.com>
2019-12-17 10:51:54 +0300

    commit

--- app/src/main/java/com/example/finlab/MainActivity.kt
+++ app/src/main/java/com/example/finlab/MainActivity.kt
@@ -2,11 +2,27 @@ package com.example.finlab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(main_toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //invalidateOptionsMenu() trigger onCreateOptionsMenu
+
+        menuInflater.inflate(R.menu.menu_main_toolbar, menu)
+
+        menu?.findItem(R.id.action_add)?.isVisible = true
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
